4371ae0b87c371d530b574d381d639a590e90778
Elf Okatan <okatanelf@gmail.com>
2019-12-10 15:46:05 +0400

    deneme1

--- app/src/main/java/com/example/artbook/MainActivity.java
+++ app/src/main/java/com/example/artbook/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
